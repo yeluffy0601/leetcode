80ae3effa274478f8d3d4b69aa7cb19b52edb225
luffy <yeluffy@qq.com>
2020-07-26 11:19:08 +0800

    add 617. 合并二叉树

--- src/MergeTwoBinaryTrees.java
+++ src/MergeTwoBinaryTrees.java
@@ -1,9 +1,125 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 617. 合并二叉树
  * https://leetcode-cn.com/problems/merge-two-binary-trees/
  */
 public class MergeTwoBinaryTrees {
 
-    // TODO: 2020/7/25
+    public static void main(String[] args) {
+        /**
+         * 输入:
+         * 	Tree 1                     Tree 2
+         *           1                         2
+         *          / \                       / \
+         *         3   2                     1   3
+         *        /                           \   \
+         *       5                             4   7
+         * 输出:
+         * 合并后的树:
+         * 	     3
+         * 	    / \
+         * 	   4   5
+         * 	  / \   \
+         * 	 5   4   7
+         */
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node3 = new TreeNode(3, null, node5);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1, node2, node3);
+        TreeNode node44 = new TreeNode(4);
+        TreeNode node11 = new TreeNode(1, node44, null);
+        TreeNode node77 = new TreeNode(7);
+        TreeNode node33 = new TreeNode(3, node77, null);
+        TreeNode node22 = new TreeNode(2, node33, node11);
+        MergeTwoBinaryTrees mtbt = new MergeTwoBinaryTrees();
+        TreeNode result = mtbt.mergeTrees(node1, node22);
+        System.out.println("node1: " + node1);
+        System.out.println("node2: " + node22);
+        System.out.println("result: " + result);
+
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        TreeNode result;
+        TreeNode t1Left = null;
+        TreeNode t1Right = null;
+        TreeNode t2Left = null;
+        TreeNode t2Right = null;
+        if (t1 == null) {
+            result = new TreeNode(t2.val);
+            t2Left = t2.left;
+            t2Right = t2.right;
+        } else if (t2 == null) {
+            result = new TreeNode(t1.val);
+            t1Left = t1.left;
+            t1Right = t1.right;
+        } else {
+            result = new TreeNode(t1.val + t2.val);
+            t1Left = t1.left;
+            t1Right = t1.right;
+            t2Left = t2.left;
+            t2Right = t2.right;
+        }
+        result.left = mergeTrees(t1Left, t2Left);
+        result.right = mergeTrees(t1Right, t2Right);
+        return result;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append(this.val);
+            List<TreeNode> context = new ArrayList<>();
+            sb.append(this.print(this, context));
+            while (context.size() != 0) {
+                sb.append(this.print(context.get(0), context));
+                context.remove(0);
+            }
+            return sb.toString();
+        }
+
+        public String print(TreeNode node, List<TreeNode> context) {
+            StringBuilder sb = new StringBuilder();
+            boolean flag = false;
+            if (node.right == null) {
+                sb.append(",null");
+            } else {
+                flag = true;
+                sb.append(",");
+                sb.append(node.right.val);
+                context.add(node.right);
+            }
+            if (node.left == null) {
+                sb.append(",null");
+            } else {
+                flag = true;
+                sb.append(",");
+                sb.append(node.left.val);
+                context.add(node.left);
+            }
+            return flag ? sb.toString() : "";
+        }
+
+    }
 
 }
--- src/RangeSumOfBst.java
+++ src/RangeSumOfBst.java
@@ -9,57 +9,57 @@ public class RangeSumOfBst {
 
     public static void main(String[] args) {
         /**
-         * 输入：root = [10,5,15,3,7,null,18], L = 7, R = 15
+         * 输入：node = [10,5,15,3,7,null,18], L = 7, R = 15
          * 输出：32
          */
-        TreeNode root3 = new TreeNode(3);
-        TreeNode root7 = new TreeNode(7);
-        TreeNode root5 = new TreeNode(5, root3, root7);
-        TreeNode root18 = new TreeNode(18);
-        TreeNode root15 = new TreeNode(15, null, root18);
-        TreeNode root10 = new TreeNode(10, root5, root15);
-        System.out.println("root1: " + root10);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node5 = new TreeNode(5, node3, node7);
+        TreeNode node18 = new TreeNode(18);
+        TreeNode node15 = new TreeNode(15, null, node18);
+        TreeNode node10 = new TreeNode(10, node5, node15);
+        System.out.println("node1: " + node10);
         RangeSumOfBst rsob = new RangeSumOfBst();
-        int result1 = rsob.rangeSumBST(root10, 7, 15);
+        int result1 = rsob.rangeSumBST(node10, 7, 15);
         System.out.println("result1: " + result1);
 
         /**
-         * 输入：root = [10,5,15,3,7,13,18,1,null,6,null], L = 6, R = 10
+         * 输入：node = [10,5,15,3,7,13,18,1,null,6,null], L = 6, R = 10
          * 输出：23
          */
-        TreeNode root1 = new TreeNode(1);
-        root3 = new TreeNode(3, root1, null);
-        TreeNode root6 = new TreeNode(6);
-        root7 = new TreeNode(7, root6, null);
-        root5 = new TreeNode(5, root3, root7);
-        TreeNode root13 = new TreeNode(13);
-        root18 = new TreeNode(18);
-        root15 = new TreeNode(15, root13, root18);
-        root10 = new TreeNode(10, root5, root15);
-        System.out.println("root2: " + root10);
-        int result2 = rsob.rangeSumBST(root10, 6, 10);
+        TreeNode node1 = new TreeNode(1);
+        node3 = new TreeNode(3, node1, null);
+        TreeNode node6 = new TreeNode(6);
+        node7 = new TreeNode(7, node6, null);
+        node5 = new TreeNode(5, node3, node7);
+        TreeNode node13 = new TreeNode(13);
+        node18 = new TreeNode(18);
+        node15 = new TreeNode(15, node13, node18);
+        node10 = new TreeNode(10, node5, node15);
+        System.out.println("node2: " + node10);
+        int result2 = rsob.rangeSumBST(node10, 6, 10);
         System.out.println("result2: " + result2);
     }
 
-    public int rangeSumBST(TreeNode root, int L, int R) {
-        if (root == null) {
+    public int rangeSumBST(TreeNode node, int L, int R) {
+        if (node == null) {
             return 0;
         }
         int result = 0;
         boolean sumRight = true;
         boolean sumLeft = true;
-        if (root.val < L) {
+        if (node.val < L) {
             sumLeft = false;
-        } else if (root.val > R) {
+        } else if (node.val > R) {
             sumRight = false;
         } else {
-            result += root.val;
+            result += node.val;
         }
-        if (sumLeft && root.left != null) {
-            result += rangeSumBST(root.left, L, R);
+        if (sumLeft && node.left != null) {
+            result += rangeSumBST(node.left, L, R);
         }
-        if (sumRight && root.right != null) {
-            result += rangeSumBST(root.right, L, R);
+        if (sumRight && node.right != null) {
+            result += rangeSumBST(node.right, L, R);
         }
         return result;
     }
